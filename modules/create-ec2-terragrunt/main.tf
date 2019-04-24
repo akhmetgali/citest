37a1c28588a2646844cc0cbbd981d766222edf45
akhmetgali <akhmetgali@outlook.com>
2019-04-24 15:38:52 +0300

    Edit circle ci and atlantis

---
+++ modules/create-ec2-terragrunt/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "test-instance" {
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "${var.subnet_id}"
+  security_groups = ["${var.security_groups}"]
+  key_name = "${var.key_name}"
+  associate_public_ip_address = "true"
+  tags {
+    Name = "CirceCI-Terraform-Test-Terragrunt",
+    Environment = "Test-and-CI",
+    New = "Atlantis"
+  }
+}
+
+output "test_ip" {
+  value = "${aws_instance.test-instance.public_ip}"
+}
+output "what" {
+  value = "ERROR OR OK"
+}
